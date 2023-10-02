25409075d15244548b66e14b78aa9fdd2af038b7
Bino26 <hbino1999@gmail.com>
2023-10-02 23:45:20 +0000

    Adding Routes, Controllers, and Middleware

---
+++ controllers/tasks.js
@@ -0,0 +1,98 @@
+const Task = require("../models/Task");
+
+const addTask = (req, res) => {
+  res.render("pages/addTask");
+};
+
+const createTask = async (req, res) => {
+  try {
+    if (req.body.complete) {
+      req.body.completed = true;
+    }
+    await Task.create(req.body);
+    req.session.pendingMessage = "The task was created.";
+    res.redirect("/tasks");
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.locals.message = Object.values(err.errors)
+        .map((item) => item.message)
+        .join(", ");
+    } else {
+      res.locals.message = "Something went wrong.";
+    }
+    res.render("pages/addTask");
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      req.session.pendingMessage = "Success! The task was deleted";
+    } else {
+      req.session.pendingMessage =
+        "An Error happen! The task you want to delete does not exist.";
+    }
+    res.redirect("/tasks");
+  } catch (err) {
+    res.locals.message = "Something went wrong.";
+    res.redirect("/tasks");
+  }
+};
+
+const editTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    res.render("pages/editTask", { task });
+  } catch (err) {
+    req.session.pendingMessage = "Something went wrong.";
+    res.redirect("/tasks");
+  }
+};
+
+const updateTask = async (req, res) => {
+  let task = false;
+  try {
+    if (req.body.complete) {
+      req.body.completed = true;
+    }
+    task = await Task.findById(req.params.id);
+    await Task.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+    });
+    req.session.pendingMessage = "The task was updated.";
+    res.redirect("/tasks");
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.locals.message = Object.values(err.errors)
+        .map((item) => item.message)
+        .join(", ");
+    } else {
+      res.locals.message = "Something went wrong.";
+    }
+    if (task) {
+      res.render("pages/editTask", { task });
+    } else {
+      req.session.pendingMessage = "Something went wrong.";
+      res.redirect("/tasks");
+    }
+  }
+};
+
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.render("pages/tasks", { tasks });
+  } catch (err) {
+    res.locals.message = "Something went wrong.";
+    res.render("pages/tasks", { tasks: [] });
+  }
+};
+module.exports = {
+  addTask,
+  createTask,
+  deleteTask,
+  updateTask,
+  editTask,
+  getTasks,
+};
--- server.js
+++ server.js
@@ -1,9 +1,58 @@
 require("dotenv").config();
 const connectDB = require("./db/connect");
+const session = require("express-session");
+const taskRouter = require("./routes/tasks");
+const setMessage = require("./middleware/message");
 
 var express = require("express");
 var app = express();
 
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use("/tasks", setMessage, taskRouter);
+
+// use res.render to load up an ejs view file
+
+// var mascots = [
+//{ name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+//{ name: 'Tux', organization: "Linux", birth_year: 1996},
+// { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+//];
+//var tagline = "No programming concept is complete without a cute animal mascot.";
+// ...  , {
+// mascots: mascots,
+//tagline: tagline
+// }); This code defines an array called mascots and a string called tagline.
+
+// index page
+app.get("/", function (req, res) {
+  var mascots = [
+    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+    { name: "Tux", organization: "Linux", birth_year: 1996 },
+    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
+  ];
+  var tagline =
+    "No programming concept is complete without a cute animal mascot.";
+
+  res.render("pages/index", {
+    mascots: mascots,
+    tagline: tagline,
+  });
+});
+
+// about page
+app.get("/about", function (req, res) {
+  res.render("pages/about");
+});
+//Launch server
 const port = 8080;
 const start = async () => {
   try {
